50f39a53d5347090b645ce64ade00f70b5e99cca
Fernando Dodino <fernando.dodino@gmail.com>
2023-08-01 19:36:34 -0300

    2023 react migration

---
+++ migraciones/migrar-react-2023.sh
@@ -0,0 +1,38 @@
+if [ ! -d $1 ];
+then
+  echo "$1: not found"
+  exit 1
+fi
+
+# git clone git@github.com:uqbar-project/$1.git
+# mv $1 $1-old
+# npm create vite@latest $1
+cd $1
+
+# Copying base files
+BASE=eg-hola-mundo-react
+cp ../$BASE/.eslint.cjs .
+cp -R ../$BASE/.github .
+cp ../$BASE/.markdownlint.json .
+echo 20.4.0 > .nvmrc
+cp ../$BASE/.prettierrc .
+cp -R ../$BASE/.vscode .
+cp ../$BASE/.gitignore .
+cp ../$BASE/vite.config.js .
+
+# Moving old project
+cp -R ../$1-old/.git .
+cp ../$1-old/README.md .
+cp -R ../$1-old/badges .
+cp -R ../$1-old/images . | true
+# index.html es nuevo, adaptarlo de public
+cp ../$1-old/public/favicon.ico public
+cp -R ../$1-old/src .
+# renaming all *.js to *.jsx
+find . -name '*.js' -exec mv {} {}x \;
+
+# Reinstall dependencies
+rm package-lock.json
+npm install -D @testing-library/react @vitest/coverage-v8 @vitejs/plugin-react jsdom prettier vitest
+
+# Revisar
