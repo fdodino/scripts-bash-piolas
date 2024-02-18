b5e93a6716fb9854b5c5fab02f7eb6652b7cc939
Fernando Dodino <fernando.dodino@gmail.com>
2024-02-18 18:48:38 -0300

    Fix kotlin migration

--- migraciones/migrar-kotlin-2024.sh
+++ migraciones/migrar-kotlin-2024.sh
@@ -8,19 +8,19 @@ cp -R ../eg-seguros-kotlin/gradle* .
 
 # Build Gradle ---------------------------------------------
 # Versión de Kotlin
-sed -i 's/1.8.10/1.9.22/g' $1/build.gradle.kts
+sed -i 's/1.8.10/1.9.22/g' ./build.gradle.kts
 
 # Versión de Kotest
-sed -i 's/5.5.5/5.8.0/g' $1/build.gradle.kts
+sed -i 's/5.5.5/5.8.0/g' ./build.gradle.kts
 
 # Versión de JDK
-sed -i 's/"17"/"21"/g' $1/build.gradle.kts
+sed -i 's/"17"/"21"/g' ./build.gradle.kts
 
 # Versión de Jacoco
-sed -i 's/0.8.8/0.8.11/g' $1/build.gradle.kts
+sed -i 's/0.8.8/0.8.11/g' ./build.gradle.kts
 
 # Versión de Mockk
-sed -i 's/1.13.4/1.13.9/g' $1/build.gradle.kts
+sed -i 's/1.13.4/1.13.9/g' ./build.gradle.kts
 
 # CI
-cp ../eg-seguros-kotlin/.github/workflows/build.yml $1/.github/workflows/build.yml
+cp ../eg-seguros-kotlin/.github/workflows/build.yml .github/workflows/build.yml
