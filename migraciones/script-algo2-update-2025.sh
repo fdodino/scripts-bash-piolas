8a81b320b010eb99bb698d3d46cfd77de723fdf7
Fernando Dodino <fernando.dodino@gmail.com>
2025-02-19 23:26:49 -0300

    Updating missing projects

--- migraciones/script-algo2-update-2025.sh
+++ migraciones/script-algo2-update-2025.sh
@@ -1,25 +1,29 @@
-# ./migrar-algo2-2025.sh eg-agricultores-kotlin master
+./migrar-algo2-2025.sh eg-agricultores-kotlin master
 ./migrar-algo2-2025.sh eg-agricultores-kotlin refactor
-
-# ./migrar-algo2-2025.sh eg-generics-kotlin master
-# ./migrar-algo2-2025.sh eg-lista-correo-kotlin master
-# ./migrar-algo2-2025.sh eg-lista-correo-kotlin 01-observers-constructor
-# ./migrar-algo2-2025.sh eg-lista-correo-kotlin 02-observers-setter
-# ./migrar-algo2-2025.sh eg-lista-correo-kotlin 03-observers-singleton
-# ./migrar-algo2-2025.sh eg-lista-correo-kotlin 04-observers-service-locator
-# ./migrar-algo2-2025.sh eg-manejo-proyectos-kotlin master
-# ./migrar-algo2-2025.sh eg-maquina-helado-kotlin master
-# ./migrar-algo2-2025.sh eg-microprocesador-kotlin master
-# ./migrar-algo2-2025.sh eg-microprocesador-kotlin creacionales
-# ./migrar-algo2-2025.sh eg-monedero-kotlin master
-# ./migrar-algo2-2025.sh eg-seleccion-personal-kotlin master
-# ./migrar-algo2-2025.sh eg-stock-refactor-kotlin master
-# ./migrar-algo2-2025.sh eg-stock-refactor-kotlin-resuelto master
-# ./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin master
-# ./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 01-builder
-# ./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 02-strategy
-# ./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 03-decorator
-# ./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 04-delegated-classes
-# ./migrar-algo2-2025.sh eg-turistas-kotlin master
+./migrar-algo2-2025.sh eg-generics-kotlin master
+./migrar-algo2-2025.sh eg-lista-correo-kotlin master
+./migrar-algo2-2025.sh eg-lista-correo-kotlin 01-observers-constructor
+./migrar-algo2-2025.sh eg-lista-correo-kotlin 02-observers-setter
+./migrar-algo2-2025.sh eg-lista-correo-kotlin 03-observers-singleton
+./migrar-algo2-2025.sh eg-lista-correo-kotlin 04-observers-service-locator
+./migrar-algo2-2025.sh eg-manejo-proyectos-kotlin master
+./migrar-algo2-2025.sh eg-maquina-helado-kotlin master
+./migrar-algo2-2025.sh eg-microprocesador-kotlin master
+./migrar-algo2-2025.sh eg-microprocesador-kotlin creacionales
+./migrar-algo2-2025.sh eg-monedero-kotlin master
+./migrar-algo2-2025.sh eg-ruletas-kotlin master
+./migrar-algo2-2025.sh eg-ruletas-kotlin stubbing-roulette
+./migrar-algo2-2025.sh eg-ruletas-kotlin stubbing-roulette-mockk
+./migrar-algo2-2025.sh eg-ruletas-kotlin mock-tests
+./migrar-algo2-2025.sh eg-seguros-kotlin master
+./migrar-algo2-2025.sh eg-seleccion-personal-kotlin master
+./migrar-algo2-2025.sh eg-stock-refactor-kotlin master
+./migrar-algo2-2025.sh eg-stock-refactor-kotlin-resuelto master
+./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin master
+./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 01-builder
+./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 02-strategy
+./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 03-decorator
+./migrar-algo2-2025.sh eg-tarjeta-credito-kotlin 04-delegated-classes
+./migrar-algo2-2025.sh eg-turistas-kotlin master
 
 # ./migrar-algo2-2025.sh eg-vehiculos-kotlin master ya migrado
